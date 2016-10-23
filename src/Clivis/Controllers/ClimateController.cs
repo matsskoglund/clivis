a988ca2938dd66104231aed1b7080a193ababd7a
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-23 14:33:53 +0200

    Started with Netatmo api

--- src/Clivis/Controllers/ClimateController.cs
+++ src/Clivis/Controllers/ClimateController.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Clivis.Models;
+using System;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace Clivis.Controllers
 {
@@ -19,7 +23,83 @@ namespace Clivis.Controllers
         [HttpGet]
         public IEnumerable<ClimateItem> GetAll()
         {
+            //string accessToken = 
+
+            string res = AuthenticateOATH(null,null,null,null);
+            
+            ClimateItem item = new ClimateItem();
+            item.Key = "tjena";
+            item.SourceName = res;
+            ClimateItems.Add(item);
             return ClimateItems.GetAll();
+
+
+        }
+
+        public static string ErrorOATHMessage = "";
+        public static string AccessToken = "";
+        public static string DeviceID = "";
+        public static string ModuleID = "";
+      
+
+
+
+        private string AuthenticateOATH(string clientUsername, string clientPassword, string clientID, string clientSecret)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                ErrorOATHMessage = "";
+
+                try
+                {
+                    HttpClient client = new HttpClient();
+
+                    var pairs = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("grant_type", "password" ),
+                        new KeyValuePair<string, string>("client_id", clientID),
+                        new KeyValuePair<string, string>( "client_secret", clientSecret),
+                        new KeyValuePair<string, string>("username", clientUsername),
+                        new KeyValuePair<string, string>( "password", clientPassword),
+                        new KeyValuePair<string, string>( "scope", "read_station read_thermostat write_thermostat")
+                    };
+
+                    var content = new FormUrlEncodedContent(pairs);
+                   
+                    response = client.PostAsync("https://api.netatmo.net/oauth2/token", content).Result;
+                }
+                catch (Exception e)
+                {
+                    ErrorOATHMessage = e.Message;
+                }
+
+                string responseDecoded = response.ToString();
+                string expiresIn = "";
+                string refreshToken = "";
+
+                //dynamic data = JsonConvert.DeserializeObject(responseDecoded);
+               // AccessToken = data.access_token;
+               // expiresIn = data.expires_in;
+               // refreshToken = data.refresh_token;
+
+          /*      lvDebug.Items.Add("Access Token: " + AccessToken);
+                lvDebug.Items.Add("Expires in: " + expiresIn);
+                lvDebug.Items.Add("Refresh Token: " + refreshToken);
+                tbAccessToken.Text = AccessToken;
+                */
+
+                if (string.IsNullOrEmpty(ErrorOATHMessage) == false)
+                {
+                   // lvDebug.Items.Add(ErrorOATHMessage);
+                }
+                return responseDecoded;
+            }
+            catch (Exception e)
+            {
+               // lvDebug.Items.Add("Error during access token retrieval: " + e.Message);
+            }
+            return "fel";
         }
 
         [HttpGet("{id}", Name = "GetClimate")]
