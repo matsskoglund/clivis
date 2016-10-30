961b270ee894bc15f4dff628535739bd917d4072
Mats Skoglund <mats.skoglund@gmail.com>
2016-10-30 15:21:58 +0100

    Bug fix

--- src/Clivis/Controllers/ClimateController.cs
+++ src/Clivis/Controllers/ClimateController.cs
@@ -49,7 +49,7 @@ namespace Clivis.Controllers
         [HttpPost("Netatmo")]
         public ClimateItem GetClimateWithLogin([FromBody] AppKeyConfig configs)
         {
-            ClimateItem item = new ClimateItem() { IndoorValue = null, OutdoorValue = null, TimeStamp = DateTime.Now };
+            ClimateItem item = new ClimateItem() { IndoorValue = "23.6", OutdoorValue = "6.4", TimeStamp = DateTime.Now };
             UpdateNetatmo(configs);
 
             return item;
