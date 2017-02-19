32ffe2952d41c0d924182ced8af7a3d8ce934035
Mats Skoglund <mats@abolint.se>
2017-02-19 16:22:45 +0100

    Fixed parameters

--- src/Clivis/dockerbuild.sh
+++ src/Clivis/dockerbuild.sh
@@ -2,8 +2,8 @@
 echo "docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER ."
 docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER .
 
-echo "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "docker login -u $DOCKER_USERNAME -p *****"
+docker login -u $DOCKER_USERNAME -p $1
 
 echo "docker push matsskoglund/clivis:$BUILD_BUILDNUMBER"
 docker push matsskoglund/clivis:$BUILD_BUILDNUMBER
