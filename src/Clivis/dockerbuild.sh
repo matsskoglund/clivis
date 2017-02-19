b5e4c9b905a93f3b6b19966d4df9b183e6fcf7d0
Mats Skoglund <mats@abolint.se>
2017-02-19 16:08:19 +0100

    Fixed the format

--- src/Clivis/dockerbuild.sh
+++ src/Clivis/dockerbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER .
-docker login -u $(Docker.UserName) -p $(Docker.PassWord)
+docker login -u $Docker_UserName -p $Docker_PassWord
 docker push matsskoglund/clivis:$BUILD_BUILDNUMBER
