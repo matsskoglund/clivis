4d50c2b276de4281c3cfadf4e17299753e0e5283
Mats Skoglund <mats@abolint.se>
2017-02-19 16:03:55 +0100

    Fixed the username variable

--- src/Clivis/dockerbuild.sh
+++ src/Clivis/dockerbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER .
-docker login -u $(Docker.Username) -p $(Docker.PassWord)
+docker login -u $(Docker.UserName) -p $(Docker.PassWord)
 docker push matsskoglund/clivis:$BUILD_BUILDNUMBER
