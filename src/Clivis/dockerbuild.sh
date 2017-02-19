15f7f39c65ae1cc249f0ed92526afa5b737696f4
Mats Skoglund <mats@abolint.se>
2017-02-19 16:15:16 +0100

    Checking

--- src/Clivis/dockerbuild.sh
+++ src/Clivis/dockerbuild.sh
@@ -2,7 +2,7 @@
 echo "docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER ."
 docker build -t matsskoglund/clivis:$BUILD_BUILDNUMBER .
 
-echo "docker login -u $DOCKER_USERNAME -p DOCKER_PASSWORD"
+echo "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 echo "docker push matsskoglund/clivis:$BUILD_BUILDNUMBER"
