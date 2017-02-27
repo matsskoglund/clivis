109ef08eead4d38492b24b74e5286872b4b6dbb5
Mats Skoglund <mats@abolint.se>
2017-02-27 11:52:50 +0100

    Started adding some logging

--- src/Clivis/Controllers/ClimateController.cs
+++ src/Clivis/Controllers/ClimateController.cs
@@ -12,31 +12,45 @@ using Microsoft.Extensions.Options;
 using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Logging;
 
 namespace Clivis.Controllers
 {
+public class LoggingEvents
+{
+    public const int GENERATE_ITEMS = 1000;
+    public const int LIST_ITEMS = 1001;
+    public const int GET_ITEM = 1002;
+    public const int INSERT_ITEM = 1003;
+    public const int UPDATE_ITEM = 1004;
+    public const int DELETE_ITEM = 1005;
 
+    public const int GET_ITEM_NOTFOUND = 4000;
+    public const int UPDATE_ITEM_NOTFOUND = 4001;
+}
     [Route("api/[controller]")]
     public class ClimateController : Controller
     {
         
         public AppKeyConfig AppConfigs { get; }
-
+        private readonly ILogger _logger;
         public IClimateSource nibe { get; }
         public IClimateSource netatmo { get; }
 
-        public ClimateController(IOptions<AppKeyConfig> configs, IDictionary<string, IClimateSource> climateSources)
+        public ClimateController(IOptions<AppKeyConfig> configs, IDictionary<string, IClimateSource> climateSources, ILogger<ClimateController> logger)
         {
 
             AppConfigs = configs.Value;
             nibe = climateSources["Nibe"];
-            netatmo = climateSources["Netatmo"];            
+            netatmo = climateSources["Netatmo"];    
+            _logger = logger;        
         }
 
         // /api/climate
         [HttpGet]
         public IActionResult GetClimate([FromQuery] string code, [FromQuery] string state)
         {
+            _logger.LogInformation(LoggingEvents.GET_ITEM, "Getting item {code}",code);
 
             HostString host = new HostString(AppConfigs.NibeRedirectURI);
             if (Request != null)
@@ -57,6 +71,7 @@ namespace Clivis.Controllers
         [HttpGet("{source}")]
         public IActionResult GetById(string source)
         {
+            _logger.LogInformation(LoggingEvents.GET_ITEM, "The source if {Source}", source);
             ClimateItem item = null;
             if (source.Equals("Nibe"))
             {
