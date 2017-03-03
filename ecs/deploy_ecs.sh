5af4b77c58ece8cb95889061f8c7c7490216900e
Mats Skoglund <mats.skoglund@gmail.com>
2017-03-03 17:56:14 +0100

    ECS bug

--- ecs/deploy_ecs.sh
+++ ecs/deploy_ecs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #terraform apply -var "aws_access_key=$AWS_ACCESS_KEY" -var "aws_secret_key=$AWS_SECRET_KEY" -var "env_netatmo_password=$ENV_NETATMO_PASSWORD" -var "env_netatmo_username=$ENV_NETATMO_USERNAME" -var "env_netatmo_clientsecret=$ENV_NETATMO_CLIENTSECRET" -var "env_netatmo_clientid=$ENV_NETATMO_CLIENTID" -var "env_nibe_clientid=$ENV_NIBE_CLIENTID" -var "env_nibe_clientsecret=$ENV_NIBE_CLIENTSECRET" -var "env_nibe_redirecturl=$ENV_NIBE_REDIRECTURL"  -var "image_id=$BUILD_BUILDID"
-terraform apply -var "aws_access_key=$1" -var "aws_secret_key=$2" -var "env_netatmo_password=$3" -var "env_netatmo_username=$4" -var "env_netatmo_clientsecret=$5" -var "env_netatmo_clientid=$6" -var "env_nibe_clientid=$7" -var "env_nibe_clientsecret=$8" -var "env_nibe_redirecturl=$9"  -var "image_id=$10"
-
-
+terraform apply -var "aws_access_key=$1" -var "aws_secret_key=$2" -var "env_netatmo_password=$3" -var "env_netatmo_username=$4" -var "env_netatmo_clientsecret=$5" -var "env_netatmo_clientid=$6" -var "env_nibe_clientid=$7" -var "env_nibe_clientsecret=$8" -var "env_nibe_redirecturl=$9"  -var "image_id=$BUILD_BUILDID"
