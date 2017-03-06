65cf1c6971a6a8be58a0762a196747ba90c60519
Mats Skoglund <mats@abolint.se>
2017-03-06 16:10:46 +0100

    Update skript

--- build_proj.sh
+++ build_proj.sh
@@ -2,5 +2,5 @@
 set -e
 dotnet restore 
 dotnet build src/Clivis/project.json
-rm -rf $(pwd)bin/Release/netcoreapp1.1/publish
-dotnet publish src/Clivis/project.json -c Release -o $(pwd)bin/Debug/netcoreapp1.1/publish
+rm -rf $(pwd)bin/Debug/netcoreapp1.1/publish
+dotnet publish src/Clivis/project.json -c Debug -o $(pwd)bin/Debug/netcoreapp1.1/publish
