6289f47a594fc7d62d3444194b883eddd0263ec6
Mats Skoglund <mats@abolint.se>
2017-03-06 15:52:28 +0100

    Changed path

--- build_proj.sh
+++ build_proj.sh
@@ -3,4 +3,4 @@ set -e
 dotnet restore 
 dotnet build src/Clivis/project.json
 rm -rf $(pwd)bin/Release/netcoreapp1.1/publish
-dotnet publish src/Clivis/project.json -c Release -o $(pwd)bin/Release/netcoreapp1.1/publish
+dotnet publish src/Clivis/project.json -c Release -o $(pwd)bin/Debug/netcoreapp1.1/publish
