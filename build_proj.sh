f385a415096b08ea8a35f817d1f205081c395e65
Mats Skoglund <mats@abolint.se>
2017-03-07 08:42:56 +0100

    Path bug

--- build_proj.sh
+++ build_proj.sh
@@ -2,6 +2,6 @@
 set -e
 dotnet restore 
 dotnet build src/Clivis/project.json
-dotnet test test/Clivis/UnitTestClivis/project.json -xml $(pwd)test/Clivis/UnitTestClivis/testresults.xml
+dotnet test test/Clivis/UnitTestsClivis/project.json -xml $(pwd)test/Clivis/UnitTestsClivis/testresults.xml
 rm -rf $(pwd)bin/Debug/netcoreapp1.1/publish
 dotnet publish src/Clivis/project.json -c Debug -o $(pwd)bin/Debug/netcoreapp1.1/publish
