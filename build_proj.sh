7b18a2da19ca052068103925005c8c8abfd44ab1
Mats Skoglund <mats@abolint.se>
2017-03-07 08:41:24 +0100

    Update dockerbuild

--- build_proj.sh
+++ build_proj.sh
@@ -2,5 +2,6 @@
 set -e
 dotnet restore 
 dotnet build src/Clivis/project.json
+dotnet test test/Clivis/UnitTestClivis/project.json -xml $(pwd)test/Clivis/UnitTestClivis/testresults.xml
 rm -rf $(pwd)bin/Debug/netcoreapp1.1/publish
 dotnet publish src/Clivis/project.json -c Debug -o $(pwd)bin/Debug/netcoreapp1.1/publish
