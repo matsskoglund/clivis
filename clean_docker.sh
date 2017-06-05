f2c99066f521620ba4d766445b25386d41f3dbc9
Mats Skoglund <mats.skoglund@scania.com>
2017-06-05 18:45:06 +0200

    Update cleaning

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop $(docker ps -q 2>/dev/null) 2>/dev/null
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+docker rmi $(docker images -q 2>/dev/null) 2>/dev/null
