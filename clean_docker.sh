a8ed47eb679ac3b642073ad40af1ca6d7feaa4da
Mats Skoglund <mats.skoglund@scania.com>
2017-06-05 18:58:49 +0200

    Added and f

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop $(docker ps -q 2>/dev/null) 2>/dev/null
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-docker rmi $(docker images -q 2>/dev/null) 2>/dev/null
+docker rmi -f $(docker images -q 2>/dev/null) 2>/dev/null
